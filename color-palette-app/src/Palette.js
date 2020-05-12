ed705f6fee017c8462479147ac89a03828eb5e10
Lilla Petri <pelvaaf@gmail.com>
2020-05-12 20:37:27 +0200

    Initial Commit

--- color-palette-app/src/Palette.js
+++ color-palette-app/src/Palette.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 
 class Palette extends Component {
     render() {
-        return ( <
-            div className = "Palette" > { /* Navbar goes here */ } <
-            div className = "Palette-colors" > { /* color boxes */ } <
-            /div> { /* footer */ } <
-            /div>
+        return(
+        <div className="Palette">
+        {/* Navbar goes here */}
+            <div className="Palette-colors">
+                {/* color boxes */}
+            </div>
+        {/* footer */}
+        </div>
         );
     }
 }
