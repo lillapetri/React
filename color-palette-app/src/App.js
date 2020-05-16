c8c1b52e1ca9834ac4bc0bc9bcab416277561a03
Lilla Petri <pelvaaf@gmail.com>
2020-05-16 18:21:24 +0200

    Add link to ColorBox, Add route

--- color-palette-app/src/App.js
+++ color-palette-app/src/App.js
@@ -33,6 +33,11 @@ class App extends Component {
             />
           )}
           />
+        <Route
+          exact
+          path='/palette/:paletteId/:colorId'
+          render={() => <h1>single color page</h1>}
+          />
         </Switch>
         )    
   }
