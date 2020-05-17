fe735fb6832b7759290b3cfb3633a99c45f12c9b
Lilla Petri <pelvaaf@gmail.com>
2020-05-17 17:48:01 +0200

    Add  NewPaletteForm

---
+++ color-palette-app/src/PaletteFormNav.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PaletteFormNav extends Component {
+    render() {
+        return (
+            <div>
+                paletteformnav
+            </div>
+        )
+    }
+}
