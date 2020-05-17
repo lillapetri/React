8329ebaaec9c1f81aeec28f38d17b5bf6def189a
Lilla Petri <pelvaaf@gmail.com>
2020-05-17 13:19:28 +0200

    Finish refactor of ColorBox

--- color-palette-app/src/ColorBox.js
+++ color-palette-app/src/ColorBox.js
@@ -3,7 +3,7 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 import {Link} from 'react-router-dom';
 import chroma from 'chroma-js';
 import { withStyles } from '@material-ui/styles';
-import './ColorBox.css';
+
 
 const styles= {
     contrastClass: {
@@ -54,7 +54,65 @@ const styles= {
         textTransform: 'uppercase',
         textDecoration: 'none',
         opacity: '0',
-        
+    },
+    boxContent: {
+        position: 'absolute',
+        width: '100%',
+        left: '0',
+        bottom: '0',
+        padding: '10px',
+        color: 'black',
+        letterSpacing: '1px',
+        fontSize: '12px',
+    },
+    copyOverlay: {
+        opacity: '0',
+        zIndex: '0',
+        width: '100%',
+        height: '100%',
+        transition: 'transform .6s ease-in-out',
+        transform: 'scale(0.1)',
+    },
+    showOverlay: {
+        opacity: '1',
+        zIndex: '10',
+        transform: 'scale(50)',
+        position: 'absolute',
+    },
+    copyMessage: {
+        position: 'fixed',
+        left: '0',
+        bottom: '20%',
+        right: '0',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: '4rem',
+        transform: 'scale(0.1)',
+        opacity: '0',
+        color: 'antiquewhite',
+        backfaceVisibility: 'hidden',
+        textShadow: '2px 2px 2px rgba(0, 0, 0, .2)',
+        '& h1': {
+            fontWeight: '400',
+            backgroundColor: 'rgb(255, 255, 255, 0.3)',
+            width: '100%',
+            padding: '1rem',
+            textAlign: 'center',
+            marginBottom: '0',
+        },
+        '& p': {
+            fontSize: '2rem',
+            fontWeight: '100'
+        }
+    },
+    showMessage: {
+        transition: 'all 0.6 ease-in-out',
+        transitionDelay: '0.2s',
+        opacity: '1',
+        transform: 'scale(1)',
+        zIndex: '25',
     }
     
 }
@@ -76,13 +134,13 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className={classes.ColorBox}>
-                    <div style={{ background }} className={`copy-overlay ${copied && 'show'}`} />
-                    <div className={`copy-msg ${copied && 'show'}`}>
+                    <div style={{ background }} className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} />
+                    <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
                         <h1 className={classes.contrastClass}>Copied!</h1>
                         <p className={classes.contrastClass}>{this.props.background}</p>
                     </div>
-                    <div className="copy-container">
-                        <div className="box-content">
+                    <div>
+                        <div className={classes.boxContent}>
                         <span className={classes.contrastClass}>{name}</span>
                         </div>
                     <button className={`${classes.copyButton} ${classes.contrastClass}`}>Copy</button>
