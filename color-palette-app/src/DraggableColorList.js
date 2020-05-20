3767dcb33c15a3ae2574bd674cb526fd00f90710
Lilla Petri <pelvaaf@gmail.com>
2020-05-20 18:34:20 +0200

    Revert "Add Draggable Colorbox"
    This reverts commit 242aa6ac6352f2d89440c9768c297f283e1e5c23.

--- color-palette-app/src/DraggableColorList.js
+++ color-palette-app/src/DraggableColorList.js
@@ -1,20 +1,11 @@
-import React from "react";
-import DraggableColorBox from "./DraggableColorBox";
-import { SortableContainer } from "react-sortable-hoc";
+import React, { Component } from 'react'
 
-const DraggableColorList = SortableContainer(({ colors, removeColor }) => {
-  return (
-    <div style={{ height: "100%" }}>
-      {colors.map((color, i) => (
-        <DraggableColorBox
-          index={i}
-          key={color.name}
-          color={color.color}
-          name={color.name}
-          handleClick={() => removeColor(color.name)}
-        />
-      ))}
-    </div>
-  );
-});
-export default DraggableColorList;
+export default class DraggableColorList extends Component {
+    render() {
+        return (
+            <div>
+                draggable color list
+            </div>
+        )
+    }
+}
