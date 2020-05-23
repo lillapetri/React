1bf9ca106ed50cce6dc52cbe6b0419b0be2c2035
Lilla Petri <pelvaaf@gmail.com>
2020-05-23 17:39:11 +0200

    Fix form dialog and install emoji picker

--- color-palette-app/src/PaletteFormNav.js
+++ color-palette-app/src/PaletteFormNav.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import classNames from "classnames";
+import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import PaletteMetaForm from "./PaletteMetaForm";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -10,113 +11,120 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-import PaletteMetaForm from './PaletteMetaForm';
 
 const drawerWidth = 400;
-
 const styles = theme => ({
-    root: {
-        display: 'flex',
-    },
-    appBar: {
-        transition: theme.transitions.create(["margin", "width"], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen
-        }),
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '64px'
-      },
-      appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(["margin", "width"], {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen
-        })
-      },
-      menuButton: {
-        marginLeft: 12,
-        marginRight: 20
-      },
-      navBtns: {
-        marginRight: '2rem',
-        '& a': {
-            textDecoration: 'none'
-        },
-      },
-      btn: {
-          margin: '0 1rem',
-          boxShadow: 'none',
-      }
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "64px"
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20
+  },
+  navBtns: {
+    marginRight: "1rem",
+    "& a": {
+      textDecoration: "none"
+    }
+  },
+  button: {
+    margin: "0 0.5rem"
+  }
 });
 
 class PaletteFormNav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            formShowing: false,
-            newPaletteName: ""
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.showForm = this.showForm.bind(this);
-    }
-    
-    handleChange(evt) {
-        this.setState({
-            [evt.target.name]: evt.target.value
-        });
-    }
-    showForm() {
-        this.setState({formShowing: true});
-    }
-    render() {
-        const { classes, open, palettes, handleSubmit } = this.props;
-        const { newPaletteName } = this.state;
-        return (
-            <div className={classes.root}>
-                <CssBaseline />
-                <AppBar
-                position='fixed'
-                color='default'
-                className={classNames(classes.appBar, {
-                    [classes.appBarShift]: open
-                })}
-                >
-                <Toolbar disableGutters={!open}>
-                    <IconButton
-                    color='inherit'
-                    aria-label='Open drawer'
-                    onClick={this.props.handleDrawerOpen}
-                    className={classNames(classes.menuButton, open && classes.hide)}
-                    >
-                    <MenuIcon />
-                    </IconButton>
-                    <Typography variant='h6' color='inherit' noWrap>
-                    Create a palette
-                    </Typography>
-                    
-                </Toolbar>
-                <div className={classes.navBtns}>
-                    <Button className={classes.btn} variant="contained" color="primary" onClick={this.showForm}>
-                    Save palette
-                    </Button>
-                    
-                    <Link to='/'>
-                        <Button className={classes.btn} variant='contained' color='secondary'>
-                        Go back
-                        </Button>
-                    </Link>
-                </div>
-                </AppBar>
-                {this.state.formShowing && (
-                    <PaletteMetaForm 
-                        palettes={palettes}
-                        handleSubmit={handleSubmit}/>
-                )}
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = { newPaletteName: "", formShowing: false };
+    this.handleChange = this.handleChange.bind(this);
+    this.showForm = this.showForm.bind(this);
+    this.hideForm = this.hideForm.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
+  showForm() {
+    this.setState({ formShowing: true });
+  }
+  hideForm() {
+    this.setState({ formShowing: false });
+  }
+  render() {
+    const { classes, open, palettes, handleSubmit } = this.props;
+    const { newPaletteName } = this.state;
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position='fixed'
+          color='default'
+          className={classNames(classes.appBar, {
+            [classes.appBarShift]: open
+          })}
+        >
+          <Toolbar disableGutters={!open}>
+            <IconButton
+              color='inherit'
+              aria-label='Open drawer'
+              onClick={this.props.handleDrawerOpen}
+              className={classNames(classes.menuButton, open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='h6' color='inherit' noWrap>
+              Create A Palette
+            </Typography>
+          </Toolbar>
+          <div className={classes.navBtns}>
+            <Link to='/'>
+              <Button
+                variant='contained'
+                color='secondary'
+                className={classes.button}
+              >
+                Go Back
+              </Button>
+            </Link>
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={this.showForm}
+              className={classes.button}
+            >
+              Save
+            </Button>
+          </div>
+        </AppBar>
+        {this.state.formShowing && (
+          <PaletteMetaForm
+            palettes={palettes}
+            handleSubmit={handleSubmit}
+            hideForm={this.hideForm}
+          />
+        )}
+      </div>
+    );
+  }
 }
 export default withStyles(styles, { withTheme: true })(PaletteFormNav);
--- color-palette-app/src/PaletteMetaForm.js
+++ color-palette-app/src/PaletteMetaForm.js
@@ -1,85 +1,84 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
 
 class PaletteMetaForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state= {
-            open: false,
-            newPaletteName: ""
-        }
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    componentDidMount() {
-        ValidatorForm.addValidationRule("isPaletteNameUnique", value =>
-            this.props.palettes.every(({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase())
-        );
-    }
-
-    handleChange(evt) {
-        this.setState({
-            [evt.target.name]: evt.target.value
-        });
-    }
-
-    handleClickOpen = () => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true,
+      newPaletteName: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    ValidatorForm.addValidationRule("isPaletteNameUnique", value =>
+      this.props.palettes.every(
+        ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
+      )
+    );
+  }
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
+  handleClickOpen = () => {
     this.setState({ open: true });
-    };
+  };
 
-    handleClose = () => {
+  handleClose = () => {
     this.setState({ open: false });
-    };
-    render() {
-        const {newPaletteName} = this.state;
-        return (
-        <Dialog
-            open={this.state.open}
-            onClose={this.handleClose}
-            aria-labelledby="form-dialog-title"
-        >
-            <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-            <ValidatorForm onSubmit={() => this.props.handleSubmit(newPaletteName)}>
-            <DialogContent>
-                <DialogContentText>
-                Please enter a name for your palette. Make sure it's unique!
-                </DialogContentText>
-                
-                    <TextValidator 
-                        label='Palette name' 
-                        name='newPaletteName'
-                        onChange={this.handleChange} 
-                        fullWidth
-                        margin='normal'
-                        value={newPaletteName}
-                        validators={['required', 'isPaletteNameUnique']}
-                        errorMessages={['Enter a palette name', 'Name already taken']}
-                    />
-                    
-                
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={this.handleClose} color="primary">
-                Cancel
-                </Button>
-                <Button 
-                    variant='contained' 
-                    type='submit'
-                    color='primary' 
-                    >Save palette
-                    </Button>
-            </DialogActions>
-            </ValidatorForm>
-        </Dialog>
-        );
-      }
-    
-}
+  };
 
+  render() {
+    const { newPaletteName } = this.state;
+    const { hideForm, handleSubmit } = this.props;
+
+    return (
+      <Dialog
+        open={this.state.open}
+        aria-labelledby='form-dialog-title'
+        onClose={hideForm}
+      >
+        <DialogTitle id='form-dialog-title'>Choose a Palette Name</DialogTitle>
+        <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
+          <DialogContent>
+            <DialogContentText>
+              Please enter a name for your new beautiful palette. Make sure it's
+              unique!
+            </DialogContentText>
+            <TextValidator
+              label='Palette Name'
+              value={newPaletteName}
+              name='newPaletteName'
+              onChange={this.handleChange}
+              fullWidth
+              margin='normal'
+              validators={["required", "isPaletteNameUnique"]}
+              errorMessages={["Enter Palette Name", "Name already used"]}
+            />
+            <Picker />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={hideForm} color='primary'>
+              Cancel
+            </Button>
+            <Button variant='contained' color='primary' type='submit'>
+              Save Palette
+            </Button>
+          </DialogActions>
+        </ValidatorForm>
+      </Dialog>
+    );
+  }
+}
 export default PaletteMetaForm;
