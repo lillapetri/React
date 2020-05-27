b0a8e7b1ecb62c505e37926f6f13f1622682373b
Lilla Petri <pelvaaf@gmail.com>
2020-05-27 19:58:59 +0200

    Add page transition

--- color-palette-app/src/App.js
+++ color-palette-app/src/App.js
@@ -7,7 +7,7 @@ import { generatePalette } from './ColorHelpers';
 import NewPaletteForm from './NewPaletteForm';
 import SingleColorPalette from './SingleColorPalette';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import './App.css';
+import Page from './Page';
 
 class App extends Component {
   constructor(props){
@@ -59,52 +59,52 @@ class App extends Component {
             exact
             path='/palette/new'
             render={(routeProps) => (
-            <div className='page'>
+            <Page>
               <NewPaletteForm 
               savePalette={this.savePalette} 
               palettes={this.state.palettes}
               {...routeProps} />
-            </div>
+            </Page>
             )}
           />
           <Route 
           exact 
           path="/" 
           render={(routeProps) => (
-            <div className='page'>
-              <PaletteList 
+            <Page>
+              <PaletteList
               palettes={this.state.palettes} 
               resetPalettes={this.resetPalettes}
               deletePalette={this.deletePalette}
               {...routeProps}/>
-            </div>
+            </Page>
           )}
           />
           <Route
             exact
             path='/palette/:id'
             render={routeProps => (
-              <div className='page'>
+              <Page>
                 <Palette
                   palette={generatePalette(
                     this.findPalette(routeProps.match.params.id)
                   )}
                 />
-              </div> 
+              </Page> 
             )}
             />
           <Route
             exact
             path='/palette/:paletteId/:colorId'
             render={routeProps => (
-              <div className='page'>
+              <Page>
                 <SingleColorPalette
                   colorId={routeProps.match.params.colorId}
                   palette={generatePalette(
                     this.findPalette(routeProps.match.params.paletteId)
                   )}
                 />
-              </div>
+              </Page>
               
             )}
             />
