9baf1ed4714ac2cdc0a1e20a2ce0366685690525
Lilla Petri <pelvaaf@gmail.com>
2020-06-03 19:14:26 +0200

    Add @material-ui

--- react-todo-app/src/Todo.js
+++ react-todo-app/src/Todo.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import CheckBox from '@material-ui/core/CheckBox';
+import IconButton from '@material-ui/core/IconButton';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+
 
 export default function Todo(props) {
-    const {todo} = props;
+    const {task, completed} = props;
     return (
-        <div>
-            <li>{todo.task}</li>
-        </div>
-    )
+        <ListItem style={{height: '64px'}}> 
+           <CheckBox checked={completed} />
+            <ListItemText style={{textDecoration: completed && 'line-through'}}>{task}</ListItemText>
+            <ListItemSecondaryAction>
+                <IconButton aria-label="Edit" >
+                    <EditIcon />
+                </IconButton>
+                <IconButton aria-label="Delete" >
+                    <DeleteIcon />
+                </IconButton>
+            </ListItemSecondaryAction>
+        </ListItem>
+    );
 }
--- react-todo-app/src/TodoList.js
+++ react-todo-app/src/TodoList.js
@@ -3,17 +3,16 @@ import Todo from './Todo';
 
 function TodoList() {
     const todos = [
-        {id: 1, task: 'This is the first todo', tags: ['tag1', 'tag2']},
-        {id: 2, task: 'This is the second todo', tags: ['tag1', 'tag3']},
-        {id: 3, task: 'This is the third todo', tags: [ 'tag2']},
-        {id: 4, task: 'This is the fourth todo', tags: ['tag2', 'tag3']}
+        {id: 1, task: 'This is the first todo', tags: ['tag1', 'tag2'], completed: true},
+        {id: 2, task: 'This is the second todo', tags: ['tag1', 'tag3'], completed: false},
+        {id: 3, task: 'This is the third todo', tags: [ 'tag2'], completed: false},
+        {id: 4, task: 'This is the fourth todo', tags: ['tag2', 'tag3'], completed: false}
 
     ]   
     return (
         <ul>
             {todos.map((todo, i) => (
-              <Todo todo={todo}
-              />
+              <Todo {...todo} />
           ))}
         </ul>
     )
