98224a2550c5d5539281b0f7b659bf40ba17ced2
Lilla Petri <pelvaaf@gmail.com>
2020-06-05 09:13:34 +0200

    Change structure to back and frontend

--- react-todo-app/src/hooks/UseToggleState.js
+++
@@ -1,9 +0,0 @@
-import {useState} from 'react';
-
-export default function useToggleState(initialVal = false) {
-    const [state, setState] = useState(initialVal);
-    const toggle = () => (
-        setState(!state)
-    );
-    return [state, toggle]
-}
