b147d84eeb63dbac33076147519408ee15c6cac9
Lilla Petri <pelvaaf@gmail.com>
2020-06-16 11:18:03 +0200

    Finish up refactoring components

--- context-app/src/App.js
+++ context-app/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Navbar from './Navbar';
 import Form from './Form';
 import PageContent from './PageContent';
-import {ThemeProvider, ThemeContext} from './contexts/ThemeContext';
-import {LanguageProvider, LanguageContext} from './contexts/LanguageContext';
+import {ThemeProvider} from './contexts/ThemeContext';
+import {LanguageProvider} from './contexts/LanguageContext';
 
 function App() {
   return (
